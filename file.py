4b582e003623d8aed1303154139d83fe1ac3cc1a
noevalergo <144984012+noevalergo@users.noreply.github.com>
2025-12-14 14:49:01 +0100

    Add functions to load and save tasks from JSON

--- file.py
+++ file.py
@@ -1,1 +1,17 @@
+import config
+import json
+
+
+def tasks_from_file():
+    """Load all tasks from the JSON data file."""
+    with open(config.TASKS_FILE, 'r') as file_object:
+        return json.load(file_object)
+
+
+def save_tasks_to_file(tasks_list):
+    """Persist tasks to the JSON data file."""
+    with open(config.TASKS_FILE, 'w') as file_object:
+        json.dump(tasks_list, file_object, indent=4)
+
+
 
